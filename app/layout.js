5cad98c4bd66753e739b10a7ca0eeb08581f30d4
rannierecamp <rannierecampos1@gmail.com>
2025-07-14 19:35:02 -0300

    fix: Usa o componente Script do Next.js para carregar o lamejs

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'sonner';
+import Script from 'next/script'; // 1. Importamos o componente de Script
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        {/* Adicionado para conversão de áudio para MP3 */}
-        <script src="/lame.min.js"></script>
+        {/* 2. Usamos o componente Script em vez da tag normal */}
+        <Script src="/lame.min.js" strategy="beforeInteractive" />
         
         {children}
         <Toaster richColors position="top-right" />
