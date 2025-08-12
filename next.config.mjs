39f01f4ac7f874df144b894e2be96cc47009805a
rannierecamp <rannierecampos1@gmail.com>
2025-08-12 14:37:14 -0300

    Corrige configuração do next.config para deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
-const withPWA = require('next-pwa')({
+import withPWA from 'next-pwa';
+
+// As configurações do seu PWA (Progressive Web App)
+const pwaConfig = {
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development', // Desativa o PWA em modo de desenvolvimento
-});
+};
 
+// As configurações do seu Next.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Suas outras configurações do Next.js podem vir aqui
-  // Por exemplo: reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+// O código abaixo une as duas configurações para você
+export default withPWA(pwaConfig)(nextConfig);
