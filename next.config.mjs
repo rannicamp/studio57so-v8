cf63ddb6b7fbf4b9a2634f5aaa97b1ec997c8699
rannierecamp <rannierecampos1@gmail.com>
2025-08-12 13:30:52 -0300

    Versão inicial do projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-/** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
-
-const nextConfig = {
-  // Suas outras configurações do Next.js podem vir aqui
-};
-
-const pwaConfig = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Desativa o PWA em modo de desenvolvimento para evitar problemas de cache
+  disable: process.env.NODE_ENV === 'development', // Desativa o PWA em modo de desenvolvimento
 });
 
-export default pwaConfig(nextConfig);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Suas outras configurações do Next.js podem vir aqui
+  // Por exemplo: reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
