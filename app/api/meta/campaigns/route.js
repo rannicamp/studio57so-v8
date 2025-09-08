87d0b391e4d4836cce05d5c8026c20e6c38e2398
rannierecamp <rannierecampos1@gmail.com>
2025-09-08 14:57:58 -0300

    fix: ajusta configuracao de auth da Meta e URIs de redirecionamento

--- app/api/meta/campaigns/route.js
+++ app/api/meta/campaigns/route.js
@@ -1,3 +1,5 @@
+//app\api\meta\campaigns\route.js
+
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../../auth/[...nextauth]/route';
