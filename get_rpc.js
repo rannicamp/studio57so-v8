7ab1ac12d2b05ceb090d02bebf01aed1bafe86db
rannicamp <rannierecampos1@gmail.com>
2026-05-25 09:16:08 -0300

    chore: atualizacao final EUA

--- get_rpc.js
+++ get_rpc.js
@@ -1,16 +1,9 @@
-const { Client } = require('pg'); 
-require('dotenv').config({ path: '.env.local' }); 
-
-const client = new Client({ connectionString: process.env.DATABASE_URL }); 
-client.connect().then(() => { 
-  client.query("SELECT pg_get_functiondef(p.oid) as def FROM pg_proc p JOIN pg_namespace n ON p.pronamespace = n.oid WHERE n.nspname = 'public' AND p.proname = 'get_lancamentos_avancado';")
-  .then(res => { 
-    if(res.rows.length > 0) {
-      console.log('RPC body length:', res.rows[0].def.length); 
-      require('fs').writeFileSync('rpc_body.sql', res.rows[0].def); 
-    } else {
-      console.log("No RPC found");
-    }
-    client.end(); 
-  }).catch(e => { console.error(e); client.end() }); 
-});
+const { Client } = require('pg');
+async function main() {
+  const client = new Client({ connectionString: 'postgresql://postgres:Srbr19010720%40@db.vhuvnutzklhskkwbpxdz.supabase.co:5432/postgres', ssl: { rejectUnauthorized: false } });
+  await client.connect();
+  const res = await client.query("SELECT prosrc FROM pg_proc WHERE proname = 'find_contact_smart'");
+  console.log(res.rows[0]?.prosrc);
+  await client.end();
+}
+main();
