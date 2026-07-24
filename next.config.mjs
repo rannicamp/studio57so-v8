417e5d384d7fa250cdabd92b6084a5a48f3e0847
Ranniere Campos <rannierecampos@studio57.arq.br>
2026-07-24 11:16:27 -0300

    fix: move headers para o nextConfig e simplifica o netlify.toml

--- next.config.mjs
+++ next.config.mjs
@@ -92,12 +92,29 @@ const nextConfig = {
         source: '/custom-sw.js',
         headers: [
           {
+            key: 'Content-Type',
+            value: 'application/javascript',
+          },
+          {
             key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
+            value: 'no-cache, no-store, must-revalidate',
           },
           {
             key: 'Service-Worker-Allowed',
             value: '/',
+          },
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache',
           },
         ],
       },
